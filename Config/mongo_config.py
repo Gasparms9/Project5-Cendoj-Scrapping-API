5b1e0ce468bf15b6584e5f0f5161aa302b8049aa
lenovo <gmasdeu.digital@gmail.com>
2022-08-16 10:06:09 +0200

    Final commit

--- Config/mongo_config.py
+++ Config/mongo_config.py
@@ -1,21 +1,5 @@
-from argparse import ONE_OR_MORE
-import pymongo
-from pymongo import MongoClient
-import sqlalchemy as alch
-import os
-import dotenv
 
-dotenv.load_dotenv()
 
-dburl = os.getenv("URL")
 
-if not dburl:
-    raise ValueError ("NO URL for database")
-
-client = MongoClient(dburl)
-db = client.get_database("Ironhack")
-harrypotter_collection = db.get_collection("HP")
-one_hp = harrypotter_collection.find_one()
-print(one_hp)
 
 
